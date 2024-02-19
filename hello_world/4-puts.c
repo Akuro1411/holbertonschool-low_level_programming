431209096dde267ecf75d076718f02f002bc201d
Akuro1411 <nahidisayev147@gmaill.com>
2024-02-19 10:48:36 +0000

    pts-5

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 /**
  *main - print sentence
- *return (0) - shows no error 
-*/
+ *return (0) - shows no error*/
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle\n");    
-    return (0);
-}
+    puts("\"Programming is like building a multilingual puzzle\n"); return (0); }
