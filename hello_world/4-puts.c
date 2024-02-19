be256f016d8a7c10de5f569bff632ad5036f07f4
Akuro1411 <nahidisayev147@gmaill.com>
2024-02-19 10:42:31 +0000

    puts-4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
+/**
+ *main - print sentence
+ *return (0) - shows no error 
+*/
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");    
-    return 0;
+    puts("\"Programming is like building a multilingual puzzle\n");    
+    return (0);
 }
