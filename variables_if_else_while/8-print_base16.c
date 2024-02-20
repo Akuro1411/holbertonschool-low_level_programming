06e5dc314509a1551aeb87a3a5fa09868ecb93cf
Aluro1411 <nahidisayev147@gmail.com>
2024-02-20 07:31:38 +0000

    hexa-2

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -9,9 +9,14 @@
 int main(void)
 {
 int n;
-for (n = 0; n < 16; n++)
+char a;
+for (n = 0; n < 10; n++)
 {
 putchar(48 + n);
+}
+for (a = 'a'; a <= 'f'; a++)
+{
+putchar(a);
 }
 putchar('\n');
 return (0);
