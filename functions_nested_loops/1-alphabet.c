657ce780c9c0469023c179f5d090cc135227611c
Akuro1411 <nahidisayev147@gmaail.com>
2024-02-21 21:12:44 +0000

    first c code with function

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+void print_alphabet(void);
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void);
+{
+print_alphabet();
+_putchar('\n')
+return (0);
+}
+void print_alphabet(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter)
+}
+}
