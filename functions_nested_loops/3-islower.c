5bc0931102cef3502472938a1f8ccedb42f95f00
Akuro1411 <nahidisayev147@gmail.com>
2024-02-22 08:50:49 +0000

    adding description for code

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,27 +1,27 @@
 #include "main.h"
 /**
- * print_alphabet - prints alphabet
- *
- *
+ * _islower - prints alphabet
+ * @c: - input of function
+ * Return: Always 0.
  */
 
 int _islower(int c)
 {
-  char i;
-  int n = 0;
-  for (i = 'a'; i < 'z'; i++)
-    {
-      if (c == i)
-	{
-	  n++;
-	}
-    }
-  if (n == 1)
-    {
-      return (1);
-    }
-  else
-    {
-      return (0);
-    }
+char i;
+int n = 0;
+for (i = 'a'; i < 'z'; i++)
+{
+if (c == i)
+{
+n++;
+}
+}
+if (n == 1)
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
