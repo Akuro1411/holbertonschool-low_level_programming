4f8e27952014090f406fa3684a32940183deefcc
Akuro1411 <nahidisayev147@gmail.com>
2024-02-22 18:21:22 +0000

    check for letter

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _isalpha - prints alphabet
+ * @c: - input of function
+ * Return: Always 0.
+ */
+
+int _isalpha(int c);
+{
+char i, j;
+int n = 0;
+for (i = 'a'; i < 'z'; i++)
+{
+for (j = 'A'; j < 'Z'; j++)
+{
+if (c == i)
+{
+n++;
+}
+else if (c == j)
+{
+n++
+}
+}
+}
+if (n == 1)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
