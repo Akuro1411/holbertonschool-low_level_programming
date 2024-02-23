206b08402f6c9ac499bcd9a75e504b2d849392cd
Akuro1411 <nahidisayev147@gmail.com>
2024-02-23 09:39:57 +0000

    wrote 6-th task

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_sign - prints alphabet
+ * @n: - input of function
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int)
+{
+  if (a > 0 || a == 0)
+    {
+      _putchar(a);
+	}
+  else if (a < 0)
+    {
+      t = a * -1;
+	_putchar(t);
+    }
+}
