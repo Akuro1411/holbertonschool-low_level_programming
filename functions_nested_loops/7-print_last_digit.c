7b91687d4d9a20cb108bc73f7dbb35ecc7c82540
Akuro1411 <nahidisayev147@gmail.com>
2024-02-23 09:56:12 +0000

    wrote the 7 th task

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints abs of number
+ * @number: - input of function
+ * Return: Always 0 (Success)
+ */
+
+int print_last_digit(int number)
+{
+  int last_digit;
+  last_digit = number % 10;
+  _putchar(last_digit);
+  return (0);
+}
