eb40e550a5961803eed50ffbbac28f1d9970f158
Akuro1411 <nahidisayev147@gmail.com>
2024-02-24 05:36:29 +0000

    aded ;

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -10,7 +10,7 @@ int print_last_digit(int number)
 {
   int last_digit;
   last_digit = number % 10;
-  _putchar(last_digit + '0')
+  _putchar(last_digit + '0');
   return (last_digit);
   return (0);
 }
