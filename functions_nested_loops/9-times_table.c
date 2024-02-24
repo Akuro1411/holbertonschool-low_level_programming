8a086dc9b64a92fe7bbe7446b0bbedb3b474797c
Akuro1411 <nahidisayev147@gmail.com>
2024-02-24 09:23:49 +0000

    added , and space

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -12,12 +12,14 @@ void times_table(void)
   for (n_of_loop = 0; n_of_loop <= 9; n_of_loop++)
     {
       number = 0;
-      while number <= 9
+      while (number <= 9)
 	{
 	  _putchar('0');
 	  t = number;
 	  for (n = 0; n < 9; n++)
 	    {
+	      _putchar(' ')
+	      _putchar(',')
 	      _putchar(t + '0');
 	      t = t + number;
 	    }
