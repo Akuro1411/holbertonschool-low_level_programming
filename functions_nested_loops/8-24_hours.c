bb8e3afaac6459f84f8bb8961741e96b3bdc98d6
Akuro1411 <nahidisayev147@gmail.com>
2024-02-24 08:48:14 +0000

    added new code style

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,54 +1,24 @@
 #include "main.h"
 
 /**
- * print_last_digit - prints abs of number
- * @number: - input of function
- * Return: Always 0 (Success)
+ * jack_bauer - prints abs of number
+ *
+ *
  */
 
 void jack_bauer(void)
 {
-  int hour, minute;
-  for (hour = 0; hour <=23; hour++)
-    {
-      for (minute = 0; minute <=59; minute++)
-	{
-	  if ((hour < 10) && (minute < 10))
-	    {
-	      _putchar('0');
-	      _putchar(hour + '0');
-	      _putchar(':');
-	      _putchar('0');
-	      _putchar(minute + '0');
-	      _putchar('\n');
-	    }
-	  else if ((hour < 10) && (minute >= 10 ))
-	    {
-	      _putchar('0');
-	      _putchar(hour + '0');
-	      _putchar(':');
-	      _putchar((minute / 10) + '0');
-	      _putchar((minute % 10) + '0');
-	      _putchar('\n');
-	    }
-	  else if ((hour >= 10) && (minute <  10))
-	    {
-	      _putchar((hour / 10) + '0');
-	      _putchar((hour % 10) + '0');
-	      _putchar(':');
-	      _putchar('0');
-	      _putchar(minute + '0');
-	      _putchar('\n');
-	    }
-	  else
-	    {
-	      _putchar((hour / 10) + '0');
-	      _putchar((hour % 10) + '0');
-	      _putchar(':');
-	      _putchar((minute / 10) + '0');
-	      _putchar((minute % 10) + '0');
-	      _putchar('\n');
-	    }
-	}
-    }
+int hour, minute;
+for (hour = 0; hour <= 23; hour++)
+{
+for (minute = 0; minute <= 59; minute++)
+{
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0');
+_putchar(':');
+_putchar((minute / 10) + '0');
+_putchar((minute % 10) + '0');
+_putchar('\n');
+}
+}
 }
