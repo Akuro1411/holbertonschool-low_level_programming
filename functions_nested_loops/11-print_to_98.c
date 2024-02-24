255ad8488c75476a9ef812b0337dd51a5c85e00a
Akuro1411 <nahidisayev147@gmail.com>
2024-02-24 14:08:21 +0000

    fixed code with while loop

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,30 +1,37 @@
 #include "main.h"
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_to_98 - check the code
+ * @number: - input of function
  */
-
 void print_to_98(int number)
 {
-  int t;
-  if (number < 98)
-    {
-      for (t = 0; t <= number; t++)
+int t;
+if (number < 98)
+{
+while (number < 98)
+{
+	if (number > 10)
 	{
-	  _putchar(t + '0');
+	_putchar((number / 10) + '0');
+	_putchar((number % 10) + '0');
 	}
-    }
-  else if (number == 98)
-    {
-      _putchar(number + '0');
-    }
-  else
-    {
-      for (t = number - 98; number >= 98; number--)
+	else
 	{
-	  _putchar(number + '0');
+	_putchar(number + '0');
 	}
-    }
+	number++
+	}
+}
+else if (number == 98)
+{
+_putchar((number / 10) + '0');
+_putchar((number % 10) + '0');
+}
+else
+{
+for (t = number - 98; number >= 98; number--)
+	{
+	_putchar((number / 10) + '0');
+	_putchar((number % 10) + '0');
+	}
 }
