df857f8e1bc876763f8bbed354f153a0d80b61af
Akuro1411 <nahidisayev147@gmail.com>
2024-02-24 09:22:10 +0000

    wrote code for 9 th task

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints abs of number
+ *
+ *
+ */
+
+void times_table(void)
+{
+  int n_of_loop, number, t;
+  for (n_of_loop = 0; n_of_loop <= 9; n_of_loop++)
+    {
+      number = 0;
+      while number <= 9
+	{
+	  _putchar('0');
+	  t = number;
+	  for (n = 0; n < 9; n++)
+	    {
+	      _putchar(t + '0');
+	      t = t + number;
+	    }
+	}
+      number++
+    }
+}
