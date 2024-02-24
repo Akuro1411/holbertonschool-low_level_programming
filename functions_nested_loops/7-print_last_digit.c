7e62b2e439e8f61feaa140e01f7be924aa883f02
Akuro1411 <nahidisayev147@gmail.com>
2024-02-24 15:32:15 +0000

    last digit with minus

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,9 +8,13 @@
 
 int print_last_digit(int number)
 {
-  int last_digit;
-  last_digit = number % 10;
-  _putchar(last_digit + '0');
-  return (last_digit);
-  return (0);
+int last_digit;
+last_digit = number % 10;
+if (last_digit < 0)
+{
+last_digit *= -1;
+}
+_putchar(last_digit + '0');
+return (last_digit);
+return (0);
 }
