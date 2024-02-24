060311c64ddf9a65935f68902c0455e37b4c2d81
Akuro1411 <nahidisayev147@gmail.com>
2024-02-24 08:22:25 +0000

    deleted 0

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -27,7 +27,7 @@ void jack_bauer(void)
 	      _putchar('0');
 	      _putchar(hour + '0');
 	      _putchar(':');
-	      return(minute);
+	      _putchar(minute);
 	      _putchar('\n');
 	    }
 	  else if ((hour >= 10) && (minute <  10))
