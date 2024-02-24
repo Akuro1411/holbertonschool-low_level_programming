68670908e4e00fb012840b497f7961e50d8f00f4
Akuro1411 <nahidisayev147@gmail.com>
2024-02-24 15:12:13 +0000

    new-4

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -20,13 +20,14 @@ void print_to_98(int number)
     }
   else
     {
-      while (number >= 98)
+      while (number > 98)
 	{
 	  printf("%d", number);
 	  _putchar(',');
 	  _putchar(' ');
 	  number--;
 	}
+      printf("%d\n", number);
 
     }
 }
