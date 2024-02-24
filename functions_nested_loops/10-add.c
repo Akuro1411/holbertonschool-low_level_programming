a9acf54df9b4fdae4d6de0866ce7e72bb46a8e47
Akuro1411 <nahidisayev147@gmail.com>
2024-02-24 13:16:23 +0000

    added code for addition

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - check the code
+ *
+ * Return: Always 0.
+ */
+
+int add(int number_1, int number_2)
+{
+  int answer;
+  answer = number_1 + number_2;
+  return (answer);
+  return (0);
+}
