efab8fcb81b9a1aa7784fd698c4241cd572cf5b5
Akuro1411 <nahidisayev147@gmail.com>
2024-02-26 08:10:48 +0000

    FizzBuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ *print_most_numbers - prints numbers 0 to 9 except 2 and 4
+ *
+ */
+
+int main(void)
+{
+  int n;
+  for (n = 1; n <= 100; n++)
+    {
+      if (n % 3 == 0)
+	{
+	  printf("Fizz");
+	}
+      else if (n % 5 == 0)
+	{
+	  printf("Buzz");
+	}
+      else if ((n % 3 == 0) && (n % 5 == 0))
+	{
+	  printf("FizzBuzz");
+	}
+      else
+	{
+	  printf("%d", n);
+	}
+    }
+  return (0);
+}
