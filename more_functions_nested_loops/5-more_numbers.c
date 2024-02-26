1e3e013223347f58f6749e27a62e36ac85827c03
Akuro1411 <nahidisayev147@gmail.com>
2024-02-26 06:45:35 +0000

    more_numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * mul - checks if uppercase letter
+ * @a: - input of function
+ * @b: - input of function
+ * Return: Always 0 (Success)
+ */
+
+void more_numbers(void)
+{
+  int n;
+  for (n = 0; n < 10; n++)
+    {
+      for (n = 0; n <= 14; n++)
+	{
+	  _putchar(n + '0')
+	}
+      _putchar('\n')
+    }
+  _putchar('\n')
+}
