b510a2561a35a1dc8449da0a76172ad89207311e
Akuro1411 <nahidisayev147@gmail.com>
2024-02-26 07:08:29 +0000

    addeddescription

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
 /**
- * print_line - prints number 0 to 14 10 times
+ * print_line - prints _ for given number's loop
+ * @n: - input of function
  */
 
 void print_line(int n)
 {
-  int t;
-  if (n <= 0)
-    {
-      _putchar('\n');
-    }
-  else
-    {
-      for (t = 0; t < n; t++)
-	{
-	  _putchar('_');
-	}
-      _putchar('\n');
-    }
+int t;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (t = 0; t < n; t++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
 }
