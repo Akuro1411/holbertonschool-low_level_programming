1c3d51b75ed524c1e8dabaeb6a5992dda50d262b
Akuro1411 <nahidisayev147@gmail.com>
2024-02-26 19:57:48 +0000

    added new for loop

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -7,7 +7,7 @@
 
 void print_triangle(int size)
 {
-int t, j;
+  int t, j, z;
 if (size <= 0)
 {
 _putchar('\n');
@@ -20,7 +20,10 @@ for (j = 1; j < t; j++)
 {
 _putchar(' ');
 }
+for (z = 1; z < t; z++)
+{
 _putchar('#');
+}
 _putchar('\n');
 }
 }
