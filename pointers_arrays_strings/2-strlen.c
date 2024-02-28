0386c7302f7dc7eb01a24c93545310e4a6aafbc6
Akuro1411 <nahidisayev147@gmail.com>
2024-02-28 06:33:31 +0000

    while loop for len of string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * swap_int - check the code
+ * @a: - input of function
+ * @b: - input of function
+ */
+
+int _strlen(char *s)
+{
+  int i;
+  i = 0;
+  while (*s)
+    {
+      i++
+    }
+  return (i);
+}
