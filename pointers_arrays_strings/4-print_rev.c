25df1a92f470b20509fc0d7085963225bd75aa1d
Akuro1411 <nahidisayev147@gmail.com>
2024-02-29 06:25:42 +0000

    print reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _puts - check the code
+ * @str: - input of function
+ * Return: Always 0 (Success)
+ */
+
+void print_rev(char *s);
+{
+    int i, t;
+    i = 0;
+    while (s[i])
+    {
+        i++;
+    }
+
+    for (t = i - 1; t >= 0; t--)
+    {
+
+        _putchar(s[t]);
+    }
+}
+
