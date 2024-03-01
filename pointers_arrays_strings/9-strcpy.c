ac188420ca2546d751e0f32619531af2510f35a1
Akuro1411 <nahidisayev147@gmail.com>
2024-03-01 06:43:27 +0000

    task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strcpy - check the code
+ * @src: - input of function
+ * @dest: - input of function
+ */
+char *_strcpy(char *dest, char *src)
+{
+  dest = src;
+  return dest;
+}
