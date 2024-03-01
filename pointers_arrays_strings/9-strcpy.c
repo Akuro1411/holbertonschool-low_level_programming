4c5c3fe4d2c7b95eec134f23533dad797544ffe4
Akuro1411 <nahidisayev147@gmail.com>
2024-03-01 07:48:57 +0000

    styled for betty

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -3,14 +3,15 @@
  * _strcpy - check the code
  * @src: - input of function
  * @dest: - input of function
+ * Return: last value (Success)
  */
 char *_strcpy(char *dest, char *src)
 {
-  int i, len;
-  len = 0;
-  while (src[len])
-    len++;
-  for (i = 0; i < len + 1; i++)
-  dest[i] = src[i];
-  return dest;
+int i, len;
+len = 0;
+while (src[len])
+len++;
+for (i = 0; i < len + 1; i++)
+dest[i] = src[i];
+return (dest);
 }
