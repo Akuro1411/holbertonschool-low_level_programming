8bdc8e3f424d65f17ae1df3855dde597f4407d05
Akuro1411 <nahidisayev147@gmail.com>
2024-03-01 09:07:28 +0000

    task-0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - check the code
+ * @str: - input of function
+ *
+ */
+char *_strcat(char *dest, char *src)
+{
+  int i, t;
+  while (src[i])
+    i++;
+  for (t = 0; t <= i; t++)
+    {
+      dest[i + t] = src[t]
+    }
+  return (dest);
+}
---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _atoi - check the code
+ * @s: - input of function
+ * Return: last value (Success)
+ */
+int _atoi(char *s);
+{
+int i, len;
+len = 0;
+while (src[len])
+len++;
+for (i = 0; i < len + 1; i++)
+dest[i] = src[i];
+return (dest);
+}
