bd3bb164ac10802de7a63e485e35e2a7b9ce5e45
Akuro1411 <nahidisayev147@gmail.com>
2024-03-01 10:42:43 +0000

    added n in loop

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strncat - check the code
+ * @dest: - input of function
+ * @src: - input of function
+ * @n: - input of function
+ * Return: Always 0 (Success)
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int a, b;
+	for (a = 0; dest[a] != '\0'; a++)
+		;
+	for (b = 0; b < n; b++)
+	{
+		dest[a + b] = src[b];
+	}
+	return (dest);
+}
