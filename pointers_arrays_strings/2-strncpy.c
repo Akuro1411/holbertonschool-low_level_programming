11ec0548962f119dc0b4679f66df22d7e6bd271d
Akuro1411 <nahidisayev147@gmail.com>
2024-03-01 12:13:57 +0000

    added n

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strcpy - check the code
+ * @src: - input of function
+ * @dest: - input of function
+ * Return: last value (Success)
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i, len;
+len = 0;
+for (i = 0; i <= n; i++)
+dest[i] = src[i];
+return (dest);
+}
