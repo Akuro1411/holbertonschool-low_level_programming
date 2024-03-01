0f7ef51bd8dafb85706b604574ae040383e2dda2
Akuro1411 <nahidisayev147@gmail.com>
2024-03-01 07:14:30 +0000

    merge 2 string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,21 +1,24 @@
 #include "main.h"
-/**
+0;276;0c/**
  * rev_string - check the code
  * @s: - input of function
  *
  */
 void rev_string(char *s)
 {
-int i, t;
+  int i, t, len;
 char new_str;
 i = 0;
+len = 0;
 while (s[i])
 {
 i++;
 }
+while (new_str[len])
+len++;
 for (t = i - 1; t >= 0; t--)
 {
-new_str++ = s[t];
+new_str[len + i] = s[t];
 }
 *s = new_str;
 }
