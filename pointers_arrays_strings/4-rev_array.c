2680981e988fffec2cceb4cf4c96734cd9d49ac3
Akuro1411 <nahidisayev147@gmail.com>
2024-03-02 12:38:21 +0000

    added l

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,5 @@
 #include "main.h"
-/**
+0;276;0c/**
  * reverse_array - check the code
  * @a: - input of function
  * @n: - input of function
@@ -10,6 +10,6 @@ void reverse_array(int *a, int n)
   int i;
   for (i = n-1; i >= 0; i--)
     {
-      return (a[i])
+      return (a[i]);
     }
 }
