69bdc00b5c1ebf4072f33b1c237ac1426cd69c4f
Akuro1411 <nahidisayev147@gmail.com>
2024-03-02 16:11:03 +0000

    loop will stop when i will be equal to n / 2

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -3,13 +3,17 @@
  * reverse_array - check the code
  * @a: - input of function
  * @n: - input of function
- * 
+ *
  */
 void reverse_array(int *a, int n)
 {
-  int i;
-  for (i = n-1; i >= 0; i--)
-    {
-      _putchar(a[i] + '0');
-    }
+int i, j, t;
+j = 0;
+for (i = n - 1; i > n / 2; i--)
+{
+t = a[i];
+a[i] = a[j];
+a[j] = t;
+j++;
+}
 }
