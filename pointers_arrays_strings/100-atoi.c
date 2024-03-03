072939c96602f7fbcb97712762286879975fd6c7
Akuro1411 <nahidisayev147@gmail.com>
2024-03-03 12:42:23 +0000

    100 atoi first try

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -4,13 +4,33 @@
  * @s: - input of function
  * Return: last value (Success)
  */
-int _atoi(char *s);
+int _atoi(char *s)
 {
-int i, len;
-len = 0;
-while (src[len])
-len++;
-for (i = 0; i < len + 1; i++)
-dest[i] = src[i];
-return (dest);
+  int i, j, t = 0, negative = 0, positive = 0;
+  int a;
+  while(s[i])
+  { if (s[i] == '-')
+    {
+      negative++;
+    }
+    else if (s[i] == '+')
+    {
+      positive++;
+    }
+    else
+    { 
+      if (s[i] >= '0' || s[i] <= '9')
+      {
+        t = t*10 + s[i] - '0';
+      }
+      else
+      continue;
+    }  
+    i++;
+  }
+  if (negative % 2 == 1)
+  {
+   t = t * (-1);
+  }
+  return t;
 }
