a87bb1b5cbac407877f65a3de423ea6c180e29c7
Akuro1411 <nahidisayev147@gmail.com>
2024-03-03 13:23:07 +0000

    fixed codde

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -4,13 +4,34 @@
  * @s: - input of function
  * Return: last value (Success)
  */
+
 int _atoi(char *s)
 {
-  int i, t = 0;
+  int i, j, t = 0, negative = 0, positive = 0;
+  int a;
   while(s[i])
-  {
-    t = t*10 + s[i] - '0';
+  { if (s[i] == '-')
+    {
+      negative++;
+    }
+    else if (s[i] == '+')
+    {
+      positive++;
+    }
+    else
+    { 
+      if (s[i] >= '0' || s[i] <= '9')
+      {
+        t = t*10 + s[i] - '0';
+      }
+      else
+      continue;
+    }  
     i++;
+  }
+  if (negative % 2 == 1)
+  {
+   t = t * (-1);
   }
   return t;
 }
