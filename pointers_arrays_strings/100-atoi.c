aa1fc91fe19fde635ea5799bbb7559ef618db412
Akuro1411 <nahidisayev147@gmail.com>
2024-03-03 12:49:56 +0000

    if is deleted

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,10 +9,7 @@ int _atoi(char *s)
   int i, t = 0;
   while(s[i])
   {
-    if (s[i] >= '0' || s[i] <= '9')
-      {
-        t = t*10 + s[i] - '0';
-      }
+    t = t*10 + s[i] - '0';
     i++;
   }
   return t;
