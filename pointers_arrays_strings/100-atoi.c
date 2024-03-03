4defbb9b45c27a43a4e88ef5ce8e4890f0cf6a0d
Akuro1411 <nahidisayev147@gmail.com>
2024-03-03 12:47:16 +0000

    deleted lines

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,35 +1,19 @@
 #include "main.h"
-/**
+11;rgb:2b2b/2b2b/2b2b/**
  * _atoi - check the code
  * @s: - input of function
  * Return: last value (Success)
  */
 int _atoi(char *s)
 {
-  int i, t = 0, negative = 0, positive = 0;
+  int i, t = 0;
   while(s[i])
-  { if (s[i] == '-')
-    {
-      negative++;
-    }
-    else if (s[i] == '+')
-    {
-      positive++;
-    }
-    else
-    { 
-      if (s[i] >= '0' || s[i] <= '9')
+  {
+    if (s[i] >= '0' || s[i] <= '9')
       {
         t = t*10 + s[i] - '0';
       }
-      else
-      continue;
-    }  
     i++;
-  }
-  if (negative % 2 == 1)
-  {
-   t = t * (-1);
   }
   return t;
 }
