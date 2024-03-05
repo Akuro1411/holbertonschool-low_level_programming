c72a18402aef4e6bcec739d53f60e6fd2b43d336
Akuro1411 <nahidisayev147@gmail.com>
2024-03-05 20:07:45 +0000

    changed structure of code

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -9,14 +9,13 @@ char *_strchr(char *s, char c)
 {
 	while (*s)
 	{
-		if (*s == c)
-		{
+		if (*s != c)
+			s++;
+		else
 			return (s);
 	}
-		else
-		s++;
-	}
 	if (c == '\0')
-	return (0);
-	return (s);
+		return (s);
+
+	return (NULL);
 }
