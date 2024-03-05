6a0817c22f2be2523e1499af54c80cc4a36467f0
Akuro1411 <nahidisayev147@gmail.com>
2024-03-05 07:43:01 +0000

    strchr own code

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strchr - checks the code
+ * @s: - input of function
+ * @c: - input of function
+ * Return: the end value of code
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+	}
+		else
+		s++;
+	}
+	if (c == '\0')
+	return (NULL);
+	return (s);
+}
