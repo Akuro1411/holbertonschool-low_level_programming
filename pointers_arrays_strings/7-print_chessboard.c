21286f6da14a8058cfcbd526dec14c32d1ffcea3
Akuro1411 <nahidisayev147@gmail.com>
2024-03-06 06:46:15 +0000

    file is formatted for betty style

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -3,18 +3,16 @@
 /**
  * print_chessboard - checks the code
  * @a: - input of function
- * @c: - input of function
- * Return: the end value of code
  */
 void print_chessboard(char (*a)[8])
 {
-  int i, j;
-  for (i = 0; i < 8; i++)
-  {
-    for (j = 0; j < 8; j++)
-    {
-      _putchar(a[i][j]);
-    }
-    _putchar('\n');
-  }
+int i, j;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+_putchar(a[i][j]);
+}
+_putchar('\n');
+}
 }
