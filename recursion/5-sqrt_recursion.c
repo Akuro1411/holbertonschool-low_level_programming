e96a9732936c195c45f988b9d47669caf4fe64c0
Akuro1411 <nahidisayev147@gmail.com>
2024-03-09 17:04:02 +0000

    designed for betty style

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,20 +1,28 @@
 #include "main.h"
+
 /**
- * _pow_recursion - check the code
- * @x: - input of number
- * @y: - input of the power
+ * find_square - check the code
+ * @number: - input of number for to find square root
+ * @checker: - starting from 1 to given number
  * Return: Returns the power of given number
  */
+
 int find_square(int checker, int number)
 {
 	if (checker * checker == number)
-		return checker;
+		return (checker);
 	else if (checker * checker > number)
-		return -1;
-	return find_square(checker + 1, number);
+		return (-1);
+	return (find_square(checker + 1, number));
 }
+
+/**
+ * _sqrt_recursion - check the code
+ * @n: - input of number for to find square root
+ * Return: the square root
+ */
 
 int _sqrt_recursion(int n)
 {
-	return find_square(1, n);
+	return (find_square(1, n));
 }
