0ae24da8199efb03a6816728107090385e2565d9
Akuro1411 <nahidisayev147@gmail.com>
2024-03-09 16:55:49 +0000

    rgb is deleted

--- recursion/main.h
+++ recursion/main.h
@@ -1,5 +1,5 @@
 int _putchar(char c);
-11;rgb:2b2b/2b2b/2b2bvoid _puts_recursion(char *s);
+void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
