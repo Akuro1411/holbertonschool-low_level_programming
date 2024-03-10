1b214fd81e2471906b56b0b43b3f36968e0f1c4c
Akuro1411 <nahidisayev147@gmail.com>
2024-03-10 09:25:21 +0000

    strpbrk code designed for betty style

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,28 +1,28 @@
 #include <stddef.h>
 #include "main.h"
 /**
- * _strchr - checks the code
+ * _strpbrk - checks the code
  * @s: - input of function
- * @c: - input of function
+ * @accept: - input of function
  * Return: the end value of code
  */
 
 char *_strpbrk(char *s, char *accept)
 {
-  int i;
-  while(*s)
-  { 
-	  i = 0;
-    while(accept[i])
-    {
-      if (*s == accept[i])
-      {
-        return s;
-      }
-      else
-      i++;
-    }
-    s++;
-  }
-  return 0;
+int i;
+while (*s)
+{
+	i = 0;
+	while (accept[i])
+	{
+		if (*s == accept[i])
+		{
+			return (s);
+		}
+		else
+		i++;
+	}
+	s++;
+}
+return (0);
 }
