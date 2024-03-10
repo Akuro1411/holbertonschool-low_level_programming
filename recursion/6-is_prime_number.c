f86e455964faf8b7d60eb9a9b523e12a38b644f8
Akuro1411 <nahidisayev147@gmail.com>
2024-03-10 17:38:19 +0000

    checker for number is prime or not

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * is_prime_number - func that returns 1 if a num is a prime num, 0 otherwise
+ * @n: number for input
+ * Return: 0
+ */
+int is_prime_number(int n)
+{
+	return (find_prime(n, 2, n / 2));
+}
+
+/**
+ * find_prime - blank
+ * @n: checkl
+ * @i: for the increment
+ * @last: limit of recursion
+ * Return: 0
+ */
+
+int find_prime(int n, int i, int last)
+{
+	if ((n % i == 0 && i <= last) || n < 0 || n == 1)
+		return (0);
+	else if (n % i != 0 && i <= last)
+		return (find_prime(n, i + 1, last));
+	else
+		return (1);
+}
