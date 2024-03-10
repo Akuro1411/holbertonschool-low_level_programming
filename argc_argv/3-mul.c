87599af6b7d376bb757514fd005f822278282e7d
Akuro1411 <nahidisayev147@gmail.com>
2024-03-10 16:13:46 +0000

    multiplication

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ * @argc: - input of function
+ * @argv: - input of the function
+ * Return: - always success
+ */
+int main(int argc, char *argv[])
+{
+	char error = "ERROR";
+	int sum;
+	if (argc < 3)
+	{
+		printf("%c", error);
+		return (1);
+	}
+	else
+	{
+		sum = atoi(argv[1]) + atoi(argv[2]);
+		printf("%d", sum);
+	}
+	return (0);
+}
