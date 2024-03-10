a23ce64c3e4e5103b94387ecc727c69d6d4eb107
Akuro1411 <nahidisayev147@gmail.com>
2024-03-10 15:38:25 +0000

    new line is added end of the number

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -7,8 +7,8 @@
  * @argv: - input of the function
  * Return: - always success
  */
-int main(int argc,char __attribute__((unused)) *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
-	printf("%d", argc - 1);
-	return 0;
+	printf("%d\n", argc - 1);
+	return (0);
 }
