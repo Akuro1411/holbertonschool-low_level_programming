6f8b524d0a9ec2ec96cc083e30ae1ab93e954561
Akuro1411 <nahidisayev147@gmail.com>
2024-03-10 15:41:35 +0000

    prints all the word on line

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ * @argc: - input of function
+ * @argv: - input of the function
+ * Return: - always success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return 0;
+}
