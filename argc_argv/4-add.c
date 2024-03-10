2d90ac6e2cc9404c81581e027815c04b198d4082
Akuro1411 <nahidisayev147@gmail.com>
2024-03-10 17:32:15 +0000

    last code

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -9,19 +9,29 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
-	for (i = 0; i < argc; i++)
+	int i, j, length, sum;
+	char *ptr;
+
+	if (argc < 2)
+		printf("0\n");
+	else
 	{
-		if (argv[i] > 'a' && argv[i] < 'Z')
+		sum = 0;
+		for (i = 1; i < argc; i++)
 		{
-			printf("ERROR\n");
-			return (1);
-			break;
+			ptr = argv[i];
+			length = strlen(ptr);
+			for (j = 0; j < length; j++)
+			{
+				if (isdigit(*(ptr + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(argv[i]);
 		}
-		else
-		{
-			sum = sum + atoi(argv[i]);
-		}
-	}
 	printf("%d\n", sum);
+	}
+	return (0);
 }
