1a478eff68761c4639f4657fd9cc36101cdb3099
Akuro1411 <nahidisayev147@gmail.com>
2024-03-13 17:22:43 +0000

    copy str

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * create_array - Creates array with given size
+ * @size: - size of array
+ * @c: - character of every element
+ * Return: Every element of array
+ */
+char *str_concat(char *s1, char *s2)
+{
+  int i = 0, j = 0, t, z;
+  char *arr;
+  while (s1[i])
+    i++;
+  while (s2[j])
+    j++;
+  arr = malloc((i + j + 2) * sizeof(char));
+  for (t = 0; s1[t] != '\0'; t++)
+  {
+    arr[t] = s1[t];
+    if (t == (i - 1))
+      for (z = 0; s2[z] != '\0'; z++)
+      {
+        arr[i] = s2[z];
+        i++;
+      };
+  }
+  return arr;
+}
