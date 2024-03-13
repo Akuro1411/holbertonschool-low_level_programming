5d4cf7d90b738879b4f64f9ed7a1d12ca62a68f7
Akuro1411 <nahidisayev147@gmail.com>
2024-03-13 08:15:12 +0000

    code is styled for betty

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -3,38 +3,29 @@
 #include <stddef.h>
 
 /**
- * create_array - Creates array with given size
- * @size: - size of array
- * @c: - character of every element
+ * _strdup - copy and return input str
+ * @str: - entered string
  * Return: Every element of array
  */
 
 char *_strdup(char *str)
 {
-  int i, length = 0;
-  char *arr;
+	int i, length = 0;
+	char *arr;
 
-  if (str == NULL)
-  {
-    return NULL;
-  }
+	if (str == NULL)
+		return (NULL);
+	while (str[length])
+		length++;
 
-  
-  while (str[length])
-  {
-    length++;
-  }
-
-  arr = malloc((length + 1) * sizeof(char));
-  if (arr == NULL)
-	  return 0;
-  for (i = 0; i < length; i++)
-  {
-    arr[i] = str[i];
-  }
-  if (arr == NULL)
-  {
-	  return NULL;
-  }
-  return arr;
+	arr = malloc((length + 1) * sizeof(char));
+	if (arr == NULL)
+		return (0);
+	for (i = 0; i < length; i++)
+	{
+		arr[i] = str[i];
+	}
+	if (arr == NULL)
+		return (NULL);
+	return (arr);
 }
