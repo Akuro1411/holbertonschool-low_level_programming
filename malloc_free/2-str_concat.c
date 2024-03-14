359dac296fd0e65d454995f262876071893f2bf9
Akuro1411 <nahidisayev147@gmail.com>
2024-03-14 06:26:59 +0000

    new code

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -10,23 +10,31 @@
 
 char *str_concat(char *s1, char *s2)
 {
-  int i = 0, j = 0, t, z;
-  char *arr;
-  while (s1[i])
-    i++;
-  while (s2[j])
-    j++;
-  arr = malloc((i + j + 3) * sizeof(char));
-  if (arr == NULL)
-	  return 0;
-  for (t = 0; s1[t] != '\0'; t++)
-  {
-    arr[t] = s1[t];
-  }
-    for (z = 0; s2[z] != '\0'; z++)
-  {
-    arr[i] = s2[z];
-    i++;
-  }
-  return arr;
+int i = 0, j = 0, t, z;
+char *arr;
+  
+if (s1 != NULL)
+while (s1[i])
+i++;;
+
+if (s2 != NULL)
+while (s2[j])
+j++;;
+
+arr = malloc((i + j + 3) * sizeof(char));
+
+if (arr == NULL)
+return 0;
+if (s1 != NULL)
+for (t = 0; s1[t] != '\0'; t++)
+{ 
+arr[t] = s1[t];
+};
+if (s2 != NULL)
+for (z = 0; s2[z] != '\0'; z++)
+{
+arr[i] = s2[z];
+i++;
+};
+return arr;
 }
