ab4e4e527328322868448d85c004a5e6c1580ff7
Akuro1411 <nahidisayev147@gmail.com>
2024-03-14 05:39:46 +0000

    is is added for empty string

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,6 +12,7 @@ char *str_concat(char *s1, char *s2)
 {
   int i = 0, j = 0, t, z;
   char *arr;
+  char empty_string[1] = "";
   while (s1[i])
     i++;
   while (s2[j])
@@ -21,6 +22,8 @@ char *str_concat(char *s1, char *s2)
 	  return 0;
   for (t = 0; s1[t] != '\0'; t++)
   {
+    if (s1[t] == empty_string[0])
+      return 0;
     arr[t] = s1[t];
     if (t == (i - 1))
       for (z = 0; s2[z] != '\0'; z++)
