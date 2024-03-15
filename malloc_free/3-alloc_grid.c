01c73e37582439e13654d968912bdf2d5aaf6f4c
Akuro1411 <nahidisayev147@gmail.com>
2024-03-15 08:28:55 +0000

    alloc grid

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * _strdup - copy and return input str
+ * @str: - entered string
+ * Return: Every element of array
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j, t;
+	if (width <=0 || height <= 0)
+	return NULL;
+	int **arr;
+
+	arr = malloc((height) * sizeof(int));
+
+	if (arr == NULL)
+		return NULL;
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc((width) * sizeof(int));
+		if (arr[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(arr[i]);
+			free(arr);
+			return NULL;
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+	return arr;
+}
