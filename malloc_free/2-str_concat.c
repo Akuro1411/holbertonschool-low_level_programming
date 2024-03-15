4cb1cec3862db099d8d2f7ccd4a7fc3af23b2f96
Akuro1411 <nahidisayev147@gmail.com>
2024-03-15 06:05:37 +0000

    code is designed for betty style

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -3,38 +3,39 @@
 #include <stddef.h>
 
 /**
- * _strdup - copy and return input str
- * @str: - entered string
+ * str_concat - concentiate first string with second
+ * @s1: - entered first string
+ * @s2: - entered second string
  * Return: Every element of array
  */
 
 char *str_concat(char *s1, char *s2)
 {
-int i = 0, j = 0, t, z;
-char *arr;
-  
-if (s1 != NULL)
-while (s1[i])
-i++;;
+	int i = 0, j = 0, t, z;
+	char *arr;
 
-if (s2 != NULL)
-while (s2[j])
-j++;;
+	if (s1 != NULL)
+		while (s1[i])
+			i++;
 
-arr = malloc((i + j + 1) * sizeof(char));
+	if (s2 != NULL)
+		while (s2[j])
+			j++;
 
-if (arr == NULL)
-return 0;
-if (s1 != NULL)
-for (t = 0; s1[t] != '\0'; t++)
-{ 
-arr[t] = s1[t];
-};
-if (s2 != NULL)
-for (z = 0; s2[z] != '\0'; z++)
-{
-arr[i] = s2[z];
-i++;
-};
-return arr;
+	arr = malloc((i + j + 1) * sizeof(char));
+
+	if (arr == NULL)
+		return (0);
+	if (s1 != NULL)
+		for (t = 0; s1[t] != '\0'; t++)
+		{
+			arr[t] = s1[t];
+		};
+	if (s2 != NULL)
+		for (z = 0; s2[z] != '\0'; z++)
+		{
+			arr[i] = s2[z];
+			i++;
+		};
+	return (arr);
 }
