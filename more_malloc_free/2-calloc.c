556ec58e45af0063a3e07b8da96261709af0217c
Akuro1411 <nahidisayev147@gmail.com>
2024-03-17 16:30:06 +0000

    code is designed for betty style

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -2,27 +2,28 @@
 #include <stdlib.h>
 
 /**
- * malloc_checked - allocate memory for given size
- * @b: - the size of memory
+ * _calloc - allocate memory for given size
+ * @nmemb: - number of elements
+ * @size: - size of elements
  * Return: returns the pointer
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-  unsigned int  i;
-  char *arr;
-  if (nmemb == 0 || size == 0)
-  {
-    return NULL;
-  }
-  arr = malloc(nmemb * size);
-  if (arr == NULL)
-  {
-    return NULL;
-  }
-   for (i = 0; i < (nmemb * size); i++)
-  {
-          arr[i] = 0;
-  }
-  return arr;
+	unsigned int  i;
+	char *arr;
+	if (nmemb == 0 || size == 0)
+	{
+		return NULL;
+	}
+	arr = malloc(nmemb * size);
+	if (arr == NULL)
+	{
+		return NULL;
+	}
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		arr[i] = 0;
+	}
+	return arr;
 }
