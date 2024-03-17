7e0eff7ec66afbbe808500d69209f068fe25b14d
Akuro1411 <nahidisayev147@gmail.com>
2024-03-17 16:06:32 +0000

    place of for statement is changed

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -15,13 +15,13 @@ void *_calloc(unsigned int nmemb, unsigned int size)
     return NULL;
   }
   arr = malloc(nmemb * size);
-  for (i = 0; i < (nmemb * size); i++)
-  {
-	  arr[i] = 0;
-  }
   if (arr == NULL)
   {
     return NULL;
+  }
+   for (i = 0; i < (nmemb * size); i++)
+  {
+          arr[i] = 0;
   }
   return arr;
 }
