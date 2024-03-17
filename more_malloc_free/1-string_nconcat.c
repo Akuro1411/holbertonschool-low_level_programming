04b18496ccee7de60d4cb887e2db59c634a7ad2c
Akuro1411 <nahidisayev147@gmail.com>
2024-03-17 14:13:09 +0000

    new code

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -9,42 +9,44 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-  unsigned int i = 0, j = 0, t = 0;
+  unsigned int i = 0, j, t = 0;
   char *arr;
   if (s1 != NULL)
-  while (s1[i])
-  {
-    i++;
-  };
+    while (s1[i])
+    {
+      i++;
+    };
   if (s2 != NULL)
-   while (s2[t])
-   {
+    while (s2[t])
+    {
       t++;
-   };
+    };
   arr = malloc((i + n + 1) * sizeof(char));
   if (arr == NULL)
   {
     return NULL;
   }
   if (s1 != NULL)
-  for(j = 0; j < i; j++)
-  {
-    arr[j] = s1[j];
-  };
+    for (j = 0; j < i; j++)
+    {
+      arr[j] = s1[j];
+    };
   if (s2 != NULL)
   {
-    if (n < t)
-      for (j = 0; j < n; j++)
-      {
-        arr[i + j] = s2[j];
-      };
-
-    else if (n >= t)
+    if (n >= t)
+    {
       for (j = 0; j < t; j++)
       {
         arr[i + j] = s2[j];
-      };
+      }
+    }
+    else 
+    {
+      for (j = 0; j < n; j++)
+      {
+        arr[i + j] = s2[j];
+      }
+    }
   }
   return arr;
-  
 }
