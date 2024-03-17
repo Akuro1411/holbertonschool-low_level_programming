f1c4949ce279e8c3c84632088b87d20586e93d50
Akuro1411 <nahidisayev147@gmail.com>
2024-03-17 16:31:35 +0000

    betty style

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,18 +12,19 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int  i;
 	char *arr;
+
 	if (nmemb == 0 || size == 0)
 	{
-		return NULL;
+		return (NULL);
 	}
 	arr = malloc(nmemb * size);
 	if (arr == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	for (i = 0; i < (nmemb * size); i++)
 	{
 		arr[i] = 0;
 	}
-	return arr;
+	return (arr);
 }
