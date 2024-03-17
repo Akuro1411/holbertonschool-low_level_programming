c15d13e2e72e7fd3b5c78b44da45958f2a79b518
Akuro1411 <nahidisayev147@gmail.com>
2024-03-17 15:36:53 +0000

    calloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memory for given size
+ * @b: - the size of memory
+ * Return: returns the pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+  unsigned int *arr, i;
+  if (nmemb == 0 || size == 0)
+  {
+    return NULL;
+  }
+  arr = malloc(size * nmemb);
+  if (arr == NULL)
+  {
+    return NULL;
+  }
+  free(arr);
+  return arr;
+}
