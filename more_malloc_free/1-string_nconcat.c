f603e0694f727a896f68fb963d3b0099b8772450
Akuro1411 <nahidisayev147@gmail.com>
2024-03-17 13:46:40 +0000

    new conditions are added to code

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -16,20 +16,34 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
   {
     i++;
   };
+  if (s2 != NULL)
+   while (s2[t])
+   {
+      t++;
+   };
   arr = malloc((i + n + 1) * sizeof(char));
   if (arr == NULL)
   {
     return NULL;
   }
+  if (s1 != NULL)
   for(j = 0; j < i; j++)
   {
     arr[j] = s1[j];
-  }
-  if (s2 != NULL)
-  for (j = 0; j < n; j++)
-  {
-    arr[i + j] = s2[j];
   };
+  if (s2 != NULL)
+  {
+    if (n < t)
+      for (j = 0; j < n; j++)
+      {
+        arr[i + j] = s2[j];
+      };
+
+    else if (n >= t)
+      for (j = 0; j < t; j++)
+      {
+        arr[i + j] = s2[j];
+      };
   return arr;
   
 }
