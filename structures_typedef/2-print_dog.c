c3f0600ce9e1a1db6d1931bf1c42f7d197972571
Akuro1411 <nahidisayev147@gmail.com>
2024-03-22 13:42:35 +0000

    new line is added end of the outputs

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,9 +13,9 @@ void print_dog(struct dog *d)
 		printf("(nil)");
 	else
 	{
-		printf("Name: %s", d->name);
-		printf("Age: %f", d->age);
-		printf("Owner: %s", d->owner);
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
 	}
 
 }
