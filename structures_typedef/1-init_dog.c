be6cf49e973a5d96bd210f633aa020ec4b830774
Akuro1411 <nahidisayev147@gmail.com>
2024-03-22 13:11:29 +0000

    initializing dog

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+/**
+ * main - check the code
+ *
+ * 
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+}
