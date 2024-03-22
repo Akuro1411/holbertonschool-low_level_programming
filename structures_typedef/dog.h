ed63c0ff35e36244393190bab2e73432b505c309
Akuro1411 <nahidisayev147@gmail.com>
2024-03-22 14:13:22 +0000

    the word wrote wrongly

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -19,7 +19,7 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
-typdef struct dog
+typedef struct dog
 {
         char *name;
         float age;
