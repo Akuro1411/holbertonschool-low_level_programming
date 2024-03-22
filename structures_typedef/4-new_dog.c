e4b4a622541abc319d1f3f319d11e6a98c7c96fb
Akuro1411 <nahidisayev147@gmail.com>
2024-03-22 16:26:30 +0000

    extra scopes are added for if

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,8 +13,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *user;
 	user = malloc(sizeof(dog_t));
 	if (user == NULL)
+	{
 		free(user);
 		return (NULL);
+	}
 	user->name = name;
 	user->age = age;
 	user->owner = owner;
