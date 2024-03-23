a342e04afddc1098899561fb8616b57153776353
Akuro1411 <nahidisayev147@gmail.com>
2024-03-23 16:26:47 +0000

    free dog

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -2,13 +2,13 @@
 #include <stddef.h>
 #include <stdlib.h>
 /**
- * print_dog - sends the value for each element
+ * free_dog - frees all allocated memory for every member
  * @d: - pointer for new variable
  */
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
-		return ;
+		return;
 	if (d->owner)
 		free(d->owner);
 	if (d->name)
