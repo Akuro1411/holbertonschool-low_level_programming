578aa21d9c772ca76ed3d90146fc9ec30038182e
Akuro1411 <nahidisayev147@gmail.com>
2024-03-24 15:57:05 +0000

    d instead of c in printf

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -16,7 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		if (va_arg(t, int) != 0)
-			printf("%c", va_arg(t, int));
+			printf("%d", va_arg(t, int));
 		else
 			printf("(nil)");
 		if (i != (n - 1))
