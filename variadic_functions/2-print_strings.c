1bace39cae7c5cfafff6ceee63b7743d6b39cd99
Akuro1411 <nahidisayev147@gmail.com>
2024-03-24 15:56:19 +0000

    0 instead of null

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -15,7 +15,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(t, n);
 	for (i = 0; i < n; i++)
 	{
-		if (va_arg(t, int) != NULL)
+		if (va_arg(t, int) != 0)
 			printf("%c", va_arg(t, int));
 		else
 			printf("(nil)");
