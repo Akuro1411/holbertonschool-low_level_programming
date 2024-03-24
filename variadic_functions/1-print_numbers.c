67b99568625a1c73dc2be99c1a639659169f7407
Akuro1411 <nahidisayev147@gmail.com>
2024-03-24 15:48:54 +0000

    int came back

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(t, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%u", va_arg(t, unsigned int));
+		printf("%d", va_arg(t, int));
 		if (i != (n - 1))
 			if (separator != NULL)
 				printf("%s", separator);
