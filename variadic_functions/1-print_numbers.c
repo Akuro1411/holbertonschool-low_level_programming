353ea4febabccbbe3105bed7f42eca894c69104f
Akuro1411 <nahidisayev147@gmail.com>
2024-03-24 15:44:18 +0000

    return 0 is deleted

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -13,17 +13,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list t;
 
-	if (n == 0)
-		return (0);
-
 	va_start(t, n);
 	for (i = 0; i < n; i++)
 	{
 		printf("%u", va_arg(t, int));
 		if (i != (n - 1))
-			if (seperator != NULL)
+			if (separator != NULL)
 				printf("%c", seperator);
 	}
 	va_end(t);
-	printf("\n")
+	printf("\n");
 }
