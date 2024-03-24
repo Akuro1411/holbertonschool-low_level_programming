c595d49d3af17431f35e6a095d99559fe8146760
Akuro1411 <nahidisayev147@gmail.com>
2024-03-24 15:28:20 +0000

    syntax is corrected

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -12,10 +12,10 @@ int (*get_op_func(char *s))(int, int)
         {NULL, NULL}
     };
     int i = 0;
-    while (ops[i]->op)
+    while ((ops + i)->op)
     {
-	    if (ops[i]->op == s)
-		    return (ops[i]->f);
+	    if ((ops + i)->op == s)
+		    return ((ops + i)->f);
     }
     printf("Error\n");
     exit(99);
