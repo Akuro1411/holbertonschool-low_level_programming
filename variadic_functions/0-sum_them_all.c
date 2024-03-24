4a1ce83f5cad29169a0a6097270436425c9e2665
Akuro1411 <nahidisayev147@gmail.com>
2024-03-24 09:45:46 +0000

    find the sum

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include "variadic_functions.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i, sum;
+	va_list t;
+
+	va_start(t, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += t;
+	}
+	va_end(t);
+	return sum
+}
