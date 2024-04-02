60142aa7297d47173f46bd75a51b8be81511b91b
Akuro1411 <nahidisayev147@gmail.com>
2024-04-02 19:24:21 +0000

    The code finds and prints length of nodes & elements

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)\n");
+	}
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+	h = h->next;
+	i++;
+	}
+	return i;
+}
