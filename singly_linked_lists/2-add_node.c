221887381885b9dcc35dd35f621df9db2e5cb7aa
Akuro1411 <nahidisayev147@gmail.com>
2024-04-02 21:29:54 +0000

    Declarations must be notice

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -9,13 +9,13 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_list = malloc(sizeof(list_t));
-	if (new_list == NULL)
-	{
-		return (0);
-	}
 	char *new_string = strdup(str);
 	int i = 0;
 
+	 if (new_list == NULL)
+        {
+                return (0);
+        }
 	new_list->next = *head;
 	new_list->str = new_string;
 	*head = new_list;
