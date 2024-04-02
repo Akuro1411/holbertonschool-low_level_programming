22cb89f1edc69949280af472a930e498dfcf7cbc
Akuro1411 <nahidisayev147@gmail.com>
2024-04-02 19:53:47 +0000

    Some changes were added to lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,7 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stddef.h>
-
+#include <stdlib.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -19,4 +19,5 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+void free_list(list_t *head);
 #endif
