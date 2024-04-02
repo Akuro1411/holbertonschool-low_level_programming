28d8342305c88c24abe9c2fab8557f1036f00ea4
Akuro1411 <nahidisayev147@gmail.com>
2024-04-02 19:52:24 +0000

    All the nodes must be free

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * list_len - prints the nodes and returns the count
+ * @h: - input for given structure
+ * Return: - Returns the length of list
+ */
+void free_list(list_t *head)
+{
+	while(head != NULL)
+	{
+		free(head->str);
+		head = head->next;
+	}
+	free(head);
+}
