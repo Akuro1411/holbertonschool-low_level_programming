8979cc54d73282da1f690f29cc84ce4270fe1392
Akuro1411 <nahidisayev147@gmail.com>
2024-04-02 19:43:52 +0000

    The name of the function was changed to new one

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- * print_list - prints the nodes and returns the count
+ * list_len - prints the nodes and returns the count
  * @h: - input for given structure
  * Return: - Returns the length of list
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	int i = 0;
 
