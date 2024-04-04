2f1c132f6500fabca2904c97905ed8c1a2875f1c
Akuro1411 <nahidisayev147@gmail.com>
2024-04-04 06:31:59 +0000

    add new element at the beginning

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * dlistint_len - prints the count of elements in list
+ * @h: - input for given structure
+ * Return: - Returns the length of list
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+  dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+  new_node->next = *head;
+  new_node->prev = NULL;
+  new_node->n = n;
+  *head = new_node;
+  return *head;
+}
