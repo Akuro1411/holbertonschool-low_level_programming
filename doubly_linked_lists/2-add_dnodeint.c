b0044e7c55997b96b43ac3d23f797d4078c279b5
Akuro1411 <nahidisayev147@gmail.com>
2024-04-06 10:20:30 +0000

    Malloc return is handled

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -9,6 +9,10 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
   dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+  if (new_node == NULL)
+  {
+    return 0;
+  }
   new_node->next = *head;
   new_node->prev = NULL;
   new_node->n = n;
