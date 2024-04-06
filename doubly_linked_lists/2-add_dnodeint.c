b0030a1d8ec8d0112e5234d91255df7646949a3a
Akuro1411 <nahidisayev147@gmail.com>
2024-04-06 12:29:24 +0000

    The code is designed for betty style

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,30 +1,32 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * dlistint_len - prints the count of elements in list
- * @h: - input for given structure
- * Return: - Returns the length of list
+ * add_dnodeint - adds the new node at the head of the list
+ * @head: - input for given structure
+ * @n: - data of new node
+ * Return: - Returns the pointer which points new node
  */
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-  dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
-  if (new_node == NULL)
-  {
-    return 0;
-  }
-  new_node->n = n;
-  new_node->prev = NULL;
-  if (*head == NULL)
-  {
-    new_node->next = NULL;
-    *head = new_node;
-  }
-  else
-  {
-    new_node->next = *head;
-    new_node->next->prev = new_node;
-    *head = new_node;
-  }
-  return *head;
+	dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+	{
+		return (0);
+	}
+	new_node->n = n;
+	new_node->prev = NULL;
+	if (*head == NULL)
+	{
+		new_node->next = NULL;
+		*head = new_node;
+	}
+	else
+	{
+		new_node->next = *head;
+		new_node->next->prev = new_node;
+		*head = new_node;
+	}
+	return (*head);
 }
