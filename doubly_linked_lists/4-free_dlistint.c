7fafd8eff6dda02382cae0fe01ac8171a18de286
Akuro1411 <nahidisayev147@gmail.com>
2024-04-06 18:47:38 +0000

    The code is designed for betty style

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,14 +1,13 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * add_dnodeint - adds the new node at the head of the list
+ * free_dlistint - frees all the nodes
  * @head: - input for given structure
- * @n: - data of new node
- * Return: - Returns the pointer which points new node
  */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *free_node = head;
+
 	while (free_node)
 	{
 		free(free_node);
