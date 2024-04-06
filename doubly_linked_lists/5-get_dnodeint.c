35c8c0f63c8edabc7ec2512182a4d698de0561b0
Akuro1411 <nahidisayev147@gmail.com>
2024-04-06 18:57:13 +0000

    gives the node on given index

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_dlistint - frees all the nodes
+ * @head: - input for given structure
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+  dlistint_t *index_node = head;
+  unsigned int i = 0;
+  while (i < index)
+  {
+    index_node = index_node->next;
+    i++;
+  }
+  return index_node;
+}
