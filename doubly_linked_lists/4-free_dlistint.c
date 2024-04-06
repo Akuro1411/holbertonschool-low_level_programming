0a6140938f19dbcfa9861c714e9ab4e7e9991fb3
Akuro1411 <nahidisayev147@gmail.com>
2024-04-06 18:44:52 +0000

    free all nodes

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * add_dnodeint - adds the new node at the head of the list
+ * @head: - input for given structure
+ * @n: - data of new node
+ * Return: - Returns the pointer which points new node
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *free_node = *head;
+	while (free_node)
+	{
+		free(free_node);
+		free_node = free_node->next;
+	}
+}
