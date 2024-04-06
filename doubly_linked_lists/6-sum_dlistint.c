ee000276a5e65eff1112ebb1a403eeb20a1a590c
Akuro1411 <nahidisayev147@gmail.com>
2024-04-06 20:14:16 +0000

    the code is designed for betty style

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,18 +1,18 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * get_dnodeint_at_index - returns the node with given index
+ * sum_dlistint - calculates the sum of elements in linked list
  * @head: - input for given structure
- * @index: - index for list
- * Return: - return the node
+ * Return: - return the sum
  */
 int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
-	while(head != NULL)
+
+	while (head != NULL)
 	{
 		sum = sum + head->n;
 		head = head->next;
 	}
-	return sum;
+	return (sum);
 }
