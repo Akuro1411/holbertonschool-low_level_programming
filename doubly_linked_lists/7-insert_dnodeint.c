1ee8dd5695602487dbe229845d047eafee3e70d1
Akuro1411 <nahidisayev147@gmail.com>
2024-04-06 20:59:39 +0000

    Add node for given index

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * sum_dlistint - calculates the sum of elements in linked list
+ * @head: - input for given structure
+ * Return: - return the sum
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+  dlistint_t *insert_node = malloc(sizeof(dlistint_t));
+  dlistint_t *node = *h;
+  unsigned int i = 0;
+  while (i < idx - 1)
+  {
+    if (node->next == NULL)
+    break;
+    node = node->next;
+    i++;
+  }
+  if (i + 1 < idx)
+  {
+    return NULL;
+  }
+
+  insert_node->next = node->next;
+  node->next = insert_node;
+  insert_node->prev = node;
+  insert_node->n = n;
+  return *h;
+}
