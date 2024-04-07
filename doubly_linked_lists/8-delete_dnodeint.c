5aa4a1fe9a432a325807c49120f3824e505622a5
Akuro1411 <nahidisayev147@gmail.com>
2024-04-07 08:58:33 +0000

    Delete node

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * sum_dlistint - calculates the sum of elements in linked list
+ * @head: - input for given structure
+ * Return: - return the sum
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+  unsigned int i = 0;
+  dlistint_t *node = *head;
+  
+  if (*head == NULL)
+  {
+    return -1;
+  }
+
+  if (index == 0)
+  {
+    if ((*head)->next == NULL)
+    {
+      *head = NULL;
+      return 1;
+    }
+    (*head)->next->prev = NULL;
+    *head = (*head)->next;
+  }
+  else
+  {
+    while(i < index  && node)
+    {
+      node = node->next;
+      i++;
+    }
+    node->prev->next = node->next;
+    node->next->prev = node->prev;
+
+  }
+  return 1;
+}
