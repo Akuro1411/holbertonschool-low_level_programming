c760b221be62032747f3336b2eb561795415780b
Akuro1411 <nahidisayev147@gmail.com>
2024-04-07 16:32:19 +0000

    insert node for given index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,65 +1,43 @@
 #include "lists.h"
-#include <stdlib.h>
 /**
- * create_node_at - creates node
- * @prev: prev node
- * @next: next node
- * @n: integer value
- *
- * Return: pointer to new node
- */
-dlistint_t *create_node_at(dlistint_t *prev, dlistint_t *next, int n)
-{
-	dlistint_t *node;
-
-	node = malloc(sizeof(dlistint_t));
-	if (node == NULL)
-		return (NULL);
-
-	node->prev = prev;
-	node->next = next;
-	node->n = n;
-
-	if (prev != NULL)
-		prev->next = node;
-
-	return (node);
-}
-
-/**
- * insert_dnodeint_at_index - inserts a node at an index
- * @h: head of list
- * @idx: index to insert at
- * @n: value to hold
- *
- * Return: pointer to new node
+ * insert_dnodeint_at_index - inserts a new node at a given position.
+ * @h: double pointer to list.
+ * @idx: index of inserting position.
+ * @n: data for new node insertion.
+ * Return: the address of the new node.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i;
-	dlistint_t *tmp, *new;
+	unsigned int i = 0;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t)), *tmp_node = *h;
 
-	if (h == NULL)
-		return (NULL);
-
-	if (idx == 0)
+	if (new_node)
 	{
-		if (*h == NULL)
-			*h = create_node_at(NULL, NULL, n);
-		else
-			*h = create_node_at((*h)->prev, *h, n);
-		return (*h);
+		new_node->n = n;
+		if (idx == 0)
+		{
+			if (*h)
+			{
+				new_node->next = *h;
+				(*h)->prev = new_node;
+			}
+			*h = new_node;
+			return (new_node);
+		}
+		while (i < idx - 1 && tmp_node)
+		{
+			tmp_node = tmp_node->next;
+			i++;
+		}
+		if (tmp_node)
+		{
+			if (tmp_node->next)
+				tmp_node->next->prev = new_node;
+			new_node->next = tmp_node->next;
+			new_node->prev = tmp_node;
+			tmp_node->next = new_node;
+			return (new_node);
+		}
 	}
-
-	tmp = *h;
-
-	for (i = 0; i < idx; i++)
-	{
-		if (tmp == NULL)
-			return (NULL);
-		tmp = tmp->next;
-	}
-
-	new = create_node_at(tmp, tmp->next, n);
-	return (new);
+	return (NULL);
 }
